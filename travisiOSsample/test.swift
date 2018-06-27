edfce36cfadf716ec3eff294aa27dc9be4f2c33d
Shreya Ganguly <shreya.ganguly99@gmail.com>
2018-06-27 21:32:34 +0530

    initial

---
+++ travisiOSsample/test.swift
@@ -0,0 +1,13 @@
+//
+//  test.swift
+//  travisiOSsample
+//
+//  Created by Shreya Ganguly on 27/06/18.
+//  Copyright © 2018 Shreya Ganguly. All rights reserved.
+//
+
+import Foundation
+
+public func hello() {
+    print("Hello World!! you are in the library")
+}
---
+++ travisiOSsample/travisiOSsample.h
@@ -0,0 +1,19 @@
+//
+//  travisiOSsample.h
+//  travisiOSsample
+//
+//  Created by Shreya Ganguly on 27/06/18.
+//  Copyright © 2018 Shreya Ganguly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for travisiOSsample.
+FOUNDATION_EXPORT double travisiOSsampleVersionNumber;
+
+//! Project version string for travisiOSsample.
+FOUNDATION_EXPORT const unsigned char travisiOSsampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <travisiOSsample/PublicHeader.h>
+
+
